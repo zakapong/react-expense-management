233bc0101794ae59833a8bccda4ebee3b37cfef2
Zakaria <zakaria.mahmud@ecsolutions.se>
2018-09-06 14:55:42 +0200

    adding edit delete and create feature

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+store.dispatch(addExpense({ description: 'Water Bill', amount:4500 }));
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+//store.dispatch(setTextFilter('water'));
 
 
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
 
+//console.log(store.getState());
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
+//ReactDOM.render(<AppRouter/>, document.getElementById('app'));
--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -174,7 +174,7 @@ store.subscribe(() => {
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
- //store.dispatch(setTextFilter('rent'));
+ store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter());
 
  store.dispatch(sortByAmount());
